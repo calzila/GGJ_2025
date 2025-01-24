20aefb7cb87c24830ce04ce498887bc332e2ba51
Callum Hancock - Mac <quaintonstarbangeriv@gmail.com>
2025-01-24 21:36:26 +0000

    Getting physics based jumping working

--- character.gd
+++ character.gd
@@ -7,15 +7,17 @@ const JUMP_VELOCITY = -700.0
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("ui_accept") and (get_contact_count() > 0):
+		print("Plz jump")
+		apply_central_force(Vector2(0,-1) * 3000000 * delta)
+		
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Vector2.ZERO
