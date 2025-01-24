5a47e727111d1556ddff3c23bae59a888c88e9fb
Callum Hancock - Mac <quaintonstarbangeriv@gmail.com>
2025-01-24 19:44:04 +0000

    Basic depth setup

--- DepthText.gd
+++ DepthText.gd
@@ -1,5 +1,6 @@
 extends RichTextLabel
 
+var depthValue
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +10,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	#depthValue = str(global_position.y)
 	
-	text = global_position.x
+	depthValue = global_position.y
+	depthValue = str(int(depthValue))
+	text = depthValue
 	
 	
 	pass
