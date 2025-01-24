559573bd7f543ec923dc547ef11904701d3a285a
Callum Hancock - Mac <quaintonstarbangeriv@gmail.com>
2025-01-24 22:28:55 +0000

    More changes

--- the_sub.gd
+++ the_sub.gd
@@ -6,25 +6,32 @@ const gravity_surfacing = -4
 
 var surfacingIntiated = false
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	var button_surfacing = $Area2D_Button	
+	
 	# Start the timer to finish. 
 	_startEndTimer()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	var velocity = linear_velocity.length()
+	print(velocity * 0.002)
+	
 	if global_position.y < 0 and surfacingIntiated:
 		# This fires when the sub comes above the surface. 
 		gravity_scale = gravity_sinking
-		print("Above the surface!!!")
 	elif global_position.y > 0 and surfacingIntiated:
 		gravity_scale = gravity_surfacing
 		
 	
 func _startEndTimer():
 	# Start a timer, once it finishes, then we want to surface the sub. 
-	await get_tree().create_timer(10).timeout
+	await get_tree().create_timer(4).timeout
 	# Timer has finished now!!!
 	print("Lift the sub now")
 	# Reverse gravity!
