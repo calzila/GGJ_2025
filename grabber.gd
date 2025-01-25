9ac0fa54a763e8cf6c909a089b9baabe92e345b8
Callum Hancock - Mac <quaintonstarbangeriv@gmail.com>
2025-01-25 15:58:40 +0000

    Adding torque when we turn

--- grabber.gd
+++ grabber.gd
@@ -10,10 +10,12 @@ var playerInRange = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if associatedLever != null:
+		associatedLever.parentGrabber = self
+	else:
+		pass
 	
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
--- lever.gd
+++ lever.gd
@@ -4,6 +4,7 @@ var playerIsHolding = false
 var inputValue = 0.0
 
 var player
+var parentGrabber
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +18,10 @@ func _process(delta: float) -> void:
 	if playerIsHolding:
 		inputValue = Input.get_axis("move_left", "move_right")
 		$PivotPoint.rotation = $PivotPoint.rotation + (inputValue * 0.5 - $PivotPoint.rotation) * (delta * 10)
+		
+		parentGrabber.theSub.apply_central_force(Vector2(inputValue,0) * 90000 * delta)
+		
+		parentGrabber.theSub.apply_torque((inputValue) * 20000000 * delta)
 		
 		if player != null:
 			player.hand_L.global_position = $PivotPoint/HandSpot_L.global_position
