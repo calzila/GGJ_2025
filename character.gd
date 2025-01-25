0a6ae5e5b61cadcd8a3cbe9787106103e28fe075
Callum Hancock - Mac <quaintonstarbangeriv@gmail.com>
2025-01-25 19:17:36 +0000

    Improving the visual style of the water

--- character.gd
+++ character.gd
@@ -49,9 +49,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		apply_central_force(direction * 100000 * delta)
 		
-		# Self-oriantate the sub
-		var correctionTorque = -rotation * 10000000 * delta
-		apply_torque(correctionTorque)
+	# Self-oriantate the sub
+	var correctionTorque = -rotation * 10000000 * delta
+	apply_torque(correctionTorque)
 	
 	# orient head to always face up
 	$Body/HeadAttachment/Head.global_rotation = $Body/HeadAttachment/Head.global_rotation + (0 - $Body/HeadAttachment/Head.global_rotation) * (delta * 7)
